b4101a9c0993144ffcf78347b9c9d71b56fc7249
Liam Kinne <kinneliam@gmail.com>
2018-08-06 01:27:15 +1000

    added A4982 stepper driver

---
+++ RM-101-Firmware/A4982.cpp
@@ -0,0 +1,37 @@
+#include "A4982.h"
+
+A4982::A4982(DigitalOutput step_output, DigitalOutput direction_output, DigitalOutput microstep_1_output, DigitalOutput microstep_2_output, DigitalOutput enable_output, DigitalOutput cs_output):
+	step_output(step_output),
+	direction_output(direction_output),
+	microstep_1_output(microstep_1_output),
+	microstep_2_output(microstep_2_output),
+	enable_output(enable_output),
+	cs_output(cs_output)
+{
+
+}
+
+void A4982::set_microstep(MicrostepResolution resolution)
+{
+	microstep_1_output.set(resolution & 0b10);
+	microstep_2_output.set(resolution & 0b01);
+}
+
+void A4982::set_enabled(bool enabled)
+{
+	enable_output.set(enabled);
+}
+
+void A4982::set_direction(Direction direction)
+{
+	direction_output.set((bool)direction);
+}
+
+void A4982::step()
+{
+	//60*1000000L/steps/microsteps/rpm
+	step_output.set(DigitalOutput::High);
+	delay_microseconds(150);
+	step_output.set(DigitalOutput::Low);
+	delay_microseconds(150);
+}
