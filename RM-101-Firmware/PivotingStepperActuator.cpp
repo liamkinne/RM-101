3ce28a6ae79ff92a4a01562cfedd8c746b22e24d
Liam Kinne <kinneliam@gmail.com>
2018-08-09 18:27:48 +1000

    make non-blocking

--- RM-101-Firmware/PivotingStepperActuator.cpp
+++ RM-101-Firmware/PivotingStepperActuator.cpp
@@ -13,15 +13,28 @@ void PivotingStepperActuator::set_steps_per_degree(double steps)
 
 void PivotingStepperActuator::set_angle_degrees(double angle)
 {
-	uint16_t steps = fabs(angle) * steps_per_degree;
+	steps_left = fabs(angle) * steps_per_degree;
 
 	if (angle >= 0)
 		motor.set_direction(A4982::Forward);
 	else
 		motor.set_direction(A4982::Reverse);
 
-	while(steps--)
-	{
+	finished = false;
+}
+
+void PivotingStepperActuator::update()
+{
+	if (steps_left > 0) {
+		steps_left--;
 		motor.step();
 	}
+	else {
+		finished = true;
+	}
+}
+
+bool PivotingStepperActuator::is_finished()
+{
+	return finished;
 }
--- RM-101-Firmware/PivotingStepperActuator.h
+++ RM-101-Firmware/PivotingStepperActuator.h
@@ -14,9 +14,18 @@ public:
 
 	void set_angle_degrees(double angle);
 
+	void update();
+
+	bool is_finished();
+
+	uint16_t steps_left;
+	bool finished;
+
 private:
 	A4982 motor;
 	double steps_per_degree;
+
+
 };
 
 #endif
