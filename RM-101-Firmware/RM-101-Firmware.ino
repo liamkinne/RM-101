3160e62c71a3e9f97bab1db20854568e6adcbad6
Liam Kinne <kinneliam@gmail.com>
2018-08-09 21:23:51 +1000

    add other motors

--- RM-101-Firmware/RM-101-Firmware.ino
+++ RM-101-Firmware/RM-101-Firmware.ino
@@ -11,7 +11,6 @@ DigitalOutput x_ms2		(IO::PORT_H, 3);
 DigitalOutput x_enable	(IO::PORT_D, 7);
 DigitalOutput x_cs		(IO::PORT_B, 0);
 A4982 x_motor(x_step, x_dir, x_ms1, x_ms2, x_enable, x_cs);
-PivotingStepperActuator lift(x_motor);
 
 DigitalOutput y_step	(IO::PORT_F, 6);
 DigitalOutput y_dir		(IO::PORT_F, 7);
@@ -20,25 +19,50 @@ DigitalOutput y_ms2		(IO::PORT_F, 4);
 DigitalOutput y_enable	(IO::PORT_F, 2);
 DigitalOutput y_cs		(IO::PORT_L, 0);
 A4982 y_motor(y_step, y_dir, y_ms1, y_ms2, y_enable, y_cs);
-PivotingStepperActuator rotate(y_motor);
 
-DigitalOutput z_step	(IO::PORT_L, 6);
+DigitalOutput z_step	(IO::PORT_L, 3);
 DigitalOutput z_dir		(IO::PORT_L, 1);
 DigitalOutput z_ms1		(IO::PORT_A, 0);
 DigitalOutput z_ms2		(IO::PORT_G, 2);
 DigitalOutput z_enable	(IO::PORT_K, 0);
 DigitalOutput z_cs		(IO::PORT_G, 1);
 A4982 z_motor(z_step, z_dir, z_ms1, z_ms2, z_enable, z_cs);
+
+DigitalOutput e0_step	(IO::PORT_A, 4);
+DigitalOutput e0_dir	(IO::PORT_A, 6);
+DigitalOutput e0_ms1	(IO::PORT_K, 1);
+DigitalOutput e0_ms2	(IO::PORT_K, 2);
+DigitalOutput e0_enable	(IO::PORT_A, 2);
+DigitalOutput e0_cs		(IO::PORT_G, 1);
+A4982 e0_motor(e0_step, e0_dir, e0_ms1, e0_ms2, e0_enable, e0_cs);
+
+DigitalOutput e1_step	(IO::PORT_C, 1);
+DigitalOutput e1_dir	(IO::PORT_C, 3);
+DigitalOutput e1_ms1	(IO::PORT_F, 3);
+DigitalOutput e1_ms2	(IO::PORT_G, 5);
+DigitalOutput e1_enable	(IO::PORT_C, 7);
+DigitalOutput e1_cs		(IO::PORT_L, 5);
+A4982 e1_motor(e1_step, e1_dir, e1_ms1, e1_ms2, e1_enable, e1_cs);
+
+PivotingStepperActuator lift(x_motor);
+PivotingStepperActuator rotate(y_motor);
 PivotingStepperActuator elbow(z_motor);
+PivotingStepperActuator grab(e0_motor);
+PivotingStepperActuator misc(e1_motor);
 
 void setup()
 {
 	x_motor.set_microstep(A4982::SixteenthStep);
 	y_motor.set_microstep(A4982::SixteenthStep);
 	z_motor.set_microstep(A4982::SixteenthStep);
+	e0_motor.set_microstep(A4982::SixteenthStep);
+	e1_motor.set_microstep(A4982::SixteenthStep);
 	lift.set_steps_per_degree(375);
 	rotate.set_steps_per_degree(375);
 	elbow.set_steps_per_degree(375);
+	grab.set_steps_per_degree(375);
+	misc.set_steps_per_degree(375);
+	misc.set_angle_degrees(-30);
 }
 
 void loop()
@@ -46,4 +70,6 @@ void loop()
 	lift.update();
 	rotate.update();
 	elbow.update();
+	grab.update();
+	misc.update();
 }
